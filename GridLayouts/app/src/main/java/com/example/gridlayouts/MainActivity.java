75e34b353b557f6efc63618eec98c3ff1b7252b5
poinwater <10836861@qq.com>
2021-03-22 21:09:29 -0700

    Basic phrases app

---
+++ GridLayouts/app/src/main/java/com/example/gridlayouts/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.gridlayouts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void playPhrases(View view){
+        Button btn = (Button) view;
+        String txt = (String) btn.getTag().toString();
+        if ((mp != null) && mp.isPlaying()){
+            mp.pause();
+        }
+        mp = MediaPlayer.create(this, getResources().getIdentifier(txt, "raw", getPackageName()));
+        mp.start();
+
+
+    }
+}
