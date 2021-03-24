d0536e66698ca8397dd84d61a52f7abbf63c57c1
poinwater <10836861@qq.com>
2021-03-24 11:19:06 -0700

    Get html from internet but only support https

--- DownloadingWebContents/app/src/main/java/com/example/downloadingwebcontents/MainActivity.java
+++ DownloadingWebContents/app/src/main/java/com/example/downloadingwebcontents/MainActivity.java
@@ -6,14 +6,40 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
 
     public class DownloadTask extends AsyncTask<String, Void, String>{
 
         @Override
         protected String doInBackground(String... strings) {
-            Log.i("URL", strings[0]);
-            return "Done";
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+
+                while (data != -1){
+                    char current = (char) data;
+
+                    result += current;
+
+                    data = reader.read();
+                }
+                return result;
+            }catch(Exception e){
+                e.printStackTrace();
+                return "Failed";
+            }
+
         }
     }
 
@@ -24,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         String result = "";
         try{
             DownloadTask task = new DownloadTask();
-            result = task.execute("http://www.zappycode.com").get();
+            result = task.execute("https://my.cityu.edu/").get();
 
         }catch(Exception e){
             e.printStackTrace();
