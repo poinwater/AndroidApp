f91c95290236bbd2db16972c0ffefb1e48f3949c
poinwater <10836861@qq.com>
2021-03-27 13:55:05 -0700

    Test for selecting menu item and updating shared preferences

--- Alert/app/src/main/java/com/example/alert/MainActivity.java
+++ Alert/app/src/main/java/com/example/alert/MainActivity.java
@@ -1,30 +1,62 @@
 package com.example.alert;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String language;
+    SharedPreferences sharedPreferences;
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        int itemId = item.getItemId();
+        switch(itemId){
+            case R.id.chinese:
+                itemSelectedAction(itemId,"chinese");
+                return true;
+            case R.id.english:
+                itemSelectedAction(itemId, "english");
+                return true;
+            default:
+                return false;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        new AlertDialog.Builder(this)
-                .setIcon(android.R.drawable.ic_dialog_alert)
-                .setTitle("Are you sure?!")
-                .setMessage("Do you really want to do this?")
-                .setPositiveButton("yes", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(getApplicationContext(),"Done!", Toast.LENGTH_SHORT).show();
-                    }
-                })
-                .setNegativeButton("no",null)
-                .show();
+        sharedPreferences = this.getSharedPreferences("com.example.alert", MODE_PRIVATE);
+        language = sharedPreferences.getString("language", "english");
+
+    }
+
+    public void itemSelectedAction (int itemId, String newLanguage) {
+
+        sharedPreferences.edit().putString("language", newLanguage).apply();
+        language = newLanguage;
+        Log.i(newLanguage, "Selected!");
+        Log.i("language", language);
+
     }
 }
