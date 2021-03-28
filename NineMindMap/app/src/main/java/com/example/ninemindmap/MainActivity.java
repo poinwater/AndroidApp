7c260542ea249361b74e7c0aab1c09ac16358b7d
poinwater <10836861@qq.com>
2021-03-28 02:26:47 -0700

    finish the interface

---
+++ NineMindMap/app/src/main/java/com/example/ninemindmap/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.ninemindmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void PlayTag(View v) {
+        String tag = v.getTag().toString();
+    }
+}
