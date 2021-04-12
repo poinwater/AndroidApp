e1cd4be7953702219ba8f6429015256e302299df
poinwater <10836861@qq.com>
2021-04-11 22:22:12 -0700

    Create recording method for long click function

--- NineMindMap/app/src/main/java/com/example/ninemindmap/MainActivity.java
+++ NineMindMap/app/src/main/java/com/example/ninemindmap/MainActivity.java
@@ -13,27 +13,31 @@ import android.media.MediaPlayer;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String LOG_TAG = "Recording";
     private static final int REQUEST_RECORD_AUDIO_PERMISSION = 200;
     protected static String Path;
-    protected static String tag;
-
-
-    protected static MediaRecorder recorder = null;
-    protected static MediaPlayer   player = null;
+    protected static String prefix = "";
+    public static boolean isRecording = false;
 
     // Requesting permission to RECORD_AUDIO
     private boolean permissionToRecordAccepted = false;
     private String [] permissions = {Manifest.permission.RECORD_AUDIO};
 
+    MediaRecorder recorder;
+    MediaPlayer audio_player;
+    MediaPlayer recording_player;
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
@@ -53,37 +57,87 @@ public class MainActivity extends AppCompatActivity {
 
         ActivityCompat.requestPermissions(this, permissions, REQUEST_RECORD_AUDIO_PERMISSION);
 
-        player = MediaPlayer.create(this, R.raw.voice);
         Path = getExternalCacheDir().getAbsolutePath();
+        createRecorder("1.3gp");
+
+        setLongClickToAll(9);
 
 
     }
 
+    public boolean startRecording (String fileName, boolean isRecording) {
+        createRecorder(fileName);
+        try {
+        recorder.prepare();
+        } catch (IOException e) {
+        Log.e(LOG_TAG, "prepare() failed");
+        return false;
+        }
+        recorder.start();
+        return true;
+    }
 
-    public static void createRecorder(String newFileName) {
+    public boolean stopRecording (String fileName, boolean isRecording) {
+        if (recorder == null) {
+            recorder.release();
+            recorder = null;
+        }
+        return false;
+    }
+
+
+    public void createRecorder(String newFileName) {
 
         recorder = new MediaRecorder();
         recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
         recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setOutputFile(Path + "/" + newFileName);
+        recorder.setOutputFile(Path + "/" + prefix + newFileName);
         recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
 
     }
 
     public void onClick(View v){
 
-        player.start();
-        int tag = Integer.parseInt(v.getTag().toString());
-        Intent intent = new Intent(getBaseContext(), Recording.class);
-        intent.putExtra("index", tag);
-        startActivity(intent);
-
+        Log.i("test", "single click" + v.getTag().toString());
 
     }
 
+    protected void setLongClickToAll(int bound) {
+        ArrayList<Button> BtnList = getAllButton(bound);
+        for (Button Btn : BtnList) {
+            setLongClick(Btn);
+        }
 
+    }
 
-    public static void PlayText(String text) {
-        Log.i("tag", text);
+    protected ArrayList<Button> getAllButton(int bound) {
+        ArrayList<Button> BtnList = new ArrayList();
+
+        for (int i = 0; i < bound; i++) {
+            int id = getResources().getIdentifier("button"+i, "id", getPackageName());
+            BtnList.add((Button) findViewById(id));
+        }
+        return BtnList;
+    }
+
+    protected void setLongClick(Button Btn) {
+        Btn.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                Log.i("test", "long click" + Btn.getText().toString());
+                String FileName = Btn.getText().toString() + ".3gp";
+
+                if (isRecording) {
+                    isRecording = stopRecording(FileName, isRecording);
+                    audio_player = MediaPlayer.create(Btn.getContext(), R.raw.recording_end);
+                    audio_player.start();
+                    return false;
+                }
+                isRecording = startRecording(FileName, isRecording);
+                audio_player = MediaPlayer.create(Btn.getContext(), R.raw.recording_start);
+                audio_player.start();
+                return true;
+            }
+        });
     }
 }
